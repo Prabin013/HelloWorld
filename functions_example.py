22282cf0c25f496a394c43bd6466c14cf0b760ae
padhikari2 <adhikariprabin001@gmail.com>
2025-01-02 16:08:30 -0700

    functions with args

---
+++ functions_example.py
@@ -0,0 +1,13 @@
+from idlelib.debugger_r import restart_subprocess_debugger
+
+
+def greet_user(first_name, last_name):
+    print(f"Hello {first_name} {last_name}")
+    print("Welcome aboard")
+
+#greet_user("John", "Smith")
+
+def square(number):
+    return number * number
+
+print(square(3))
